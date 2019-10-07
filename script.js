bd71d8347887576d9027a0b0ac1a1d82ef2ab15e
leonidleonid <36963539+leonidleonid@users.noreply.github.com>
2019-10-07 11:21:50 +0300

    sort items added

--- script.js
+++ script.js
@@ -3,97 +3,150 @@ const DATA = [
         id: 1,
         title: "Выступление DJ Flexbox",
         description: "Новая программа от легенды танцевальной музыки и верстки сайтов в рамках мирового тура",
-        price: 300,
+        price: 1300,
         type: "concert"
     },
     {
         id: 2,
-        title: "Выступление DJ Flexbox",
+        title: "Фестиваль красок",
         description: "Новая программа от легенды танцевальной музыки и верстки сайтов в рамках мирового тура",
-        price: 300,
-        type: "concert"
+        price: 750,
+        type: "festival"
     },
     {
         id: 3,
-        title: "Выступление DJ Flexbox",
+        title: "Стендап-шоу \"Открытый микрофон\"",
         description: "Новая программа от легенды танцевальной музыки и верстки сайтов в рамках мирового тура",
-        price: 300,
+        price: 800,
         type: "concert"
     },
     {
         id: 4,
-        title: "Выступление DJ Flexbox",
+        title: "Классика рока в Ледовом",
         description: "Новая программа от легенды танцевальной музыки и верстки сайтов в рамках мирового тура",
-        price: 300,
+        price: 1100,
         type: "concert"
     },
     {
         id: 5,
-        title: "Выступление DJ Flexbox",
+        title: "Выставка современной фотографии",
         description: "Новая программа от легенды танцевальной музыки и верстки сайтов в рамках мирового тура",
-        price: 300,
-        type: "concert"
+        price: 900,
+        type: "exhibition"
     },
     {
         id: 6,
-        title: "Выступление DJ Flexbox",
+        title: "Кубок по моторалли",
         description: "Новая программа от легенды танцевальной музыки и верстки сайтов в рамках мирового тура",
-        price: 300,
-        type: "concert"
+        price: 1600,
+        type: "sport"
     },
     {
         id: 7,
-        title: "Выступление DJ Flexbox",
+        title: "Выступление DJ Sass и DJ Less",
         description: "Новая программа от легенды танцевальной музыки и верстки сайтов в рамках мирового тура",
-        price: 300,
+        price: 1500,
         type: "concert"
     },
     {
         id: 8,
-        title: "Выступление DJ Flexbox",
+        title: "Drift racing",
         description: "Новая программа от легенды танцевальной музыки и верстки сайтов в рамках мирового тура",
-        price: 300,
-        type: "concert"
+        price: 2000,
+        type: "sport"
     },
     {
         id: 9,
-        title: "Выступление DJ Flexbox",
+        title: "Рок-фестиваль",
         description: "Новая программа от легенды танцевальной музыки и верстки сайтов в рамках мирового тура",
-        price: 300,
+        price: 3000,
         type: "concert"
     },
 ];
 
+class Menu {
+    constructor() {
+        let self = this;
+        this.sortActivated = false;
+        this.sortBtn = document.querySelector('#sortBtn');
+        this.sortBtn.onclick = function() {
+            document.dispatchEvent(new Event('sortBtn_activated'));
+            self.sortActivated = !self.sortActivated;
+            if (self.sortActivated) {
+                self.sortBtn.innerText = 'Оменить сортировку';
+            } else {
+                self.sortBtn.innerText = 'Сортировать по цене';
+            }
+        }
+    }
+}
+
 class Gallery {
     constructor(data) {
+        let self = this;
+        this.gallery = document.querySelector('.gallery');
         if (data instanceof Array && data.length > 0) {
             this.data = data;
+            this.isSorted = false;
+            this.renderCards();
         } else {
-            this.data = [];
+            this.gallery.innerText = "События не найдены";
         }
+
+        document.addEventListener('sortBtn_activated', function () {
+            if (self.isSorted) {
+                self.gallery.innerHTML = '';
+                self.renderCards();
+            } else {
+                self.sortCardsByPrice();
+            }
+            self.isSorted = !self.isSorted;
+        });
+    }
+
+    renderCards() {
         for (let item of this.data) {
-            Gallery.renderCard(item);
+            this.renderCard(item);
         }
     }
 
-    static renderCard(itemData) {
-        let gallery = document.querySelector('.gallery');
+    renderCard(itemData) {
         let card = document.createElement("div");
         card.className = "card";
         card.setAttribute('event_type', itemData.type);
         card.innerHTML = `
-            <img class="card__cover" src="./img/cover_of_card_${itemData.id}.jpg">
+            <img class="card__cover" src="./img/cover_of_card_${itemData.id}.gif">
             <div class="card__info">
                 <div class="card__title">
                     <p>${itemData.title}</p>
+                </div>
+                <div class="card__price">
+                    <p>${itemData.price} &#8381</p>
                 </div>
                 <div class="card__description">
                     <p>${itemData.description}</p>
                 </div>
             </div>`;
-        gallery.appendChild(card);
+        this.gallery.appendChild(card);
+    }
+
+    sortCardsByPrice() {
+        if (this.sortedData === undefined) {
+            this.sortedData = this.data.slice();
+            this.sortedData.sort((item1, item2) => {
+                if (item1['price'] < item2['price'])
+                    return -1;
+                else
+                    return 1
+            });
+        }
+        this.gallery.innerHTML = '';
+        for (let item of this.sortedData) {
+            this.renderCard(item);
+        }
     }
 
 }
 
 let gallery = new Gallery(DATA);
+let menu = new Menu();
